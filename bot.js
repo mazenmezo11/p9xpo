b9ecc0350e2c235a8ca8d1bb8c4c4f27379c8fbe
mazenmezo11 <46426920+mazenmezo11@users.noreply.github.com>
2019-01-06 22:56:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -75,7 +75,7 @@ client.on('guildMemberAdd', Sal => { //By Salto7#4595
     .setColor("#707070")
     .addField(': دخولك لديسكورد قبل', ${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \${moment(heg.createdTimestamp).fromNow()}`,true) 
     .addField(': انضمامك لسيرفر قبل',${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n `${moment(h.joinedAt).fromNow()}`, true)               
-    .setFooter(OverBot`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif%27)
+    .setFooter(OverBot`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif%27')
     .setThumbnail(heg.avatarURL);
     message.channel.send(id)
 }
